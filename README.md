6f85a7beda298f3b9a356b15149592d14681a2cf
qiangzhouliang <2538096489@qq.com>
2019-08-28 19:14:43 +0800

    添加顶部导航栏效果

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 初始化项目，添加底部导航效果
 # 2 swiper
 使用swiper添加首页轮播图效果
+# 3 appbar_slide_gradul
+## 3.1 使用到的主要组件
+- 1 Stack 层叠组件，前面的元素在上面，后面的元素在下面
+- 2 MediaQuery 具有删除padding的属性
+- 3 NotificationListener 可以监听列表的滚动
+- 4 Opacity包裹，可以改变组件透明度
+- 5 decoration 装饰器, 背景色为白色
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
-
+//滚动的最大值,阈值
+const APPBAR_SCROLL_OFFSET = 100;
 /**
  * 首页界面
  */
@@ -17,35 +19,93 @@ class _HomePageState extends State<HomePage> {
     'http://pic1.win4000.com/wallpaper/c/53cdd1f7c1f21.jpg',
     'http://pic16.nipic.com/20111006/6239936_092702973000_2.jpg'
   ];
+  //appbar透明度
+  double appBarAlpha = 0;
+  //滚动处理操作
+  _onScroll(offset){
+    double alpha = offset / APPBAR_SCROLL_OFFSET;
+    if(alpha < 0){
+      //向上滚动
+      alpha = 0;
+    }else if(alpha > 1){
+      //向下滚动
+      alpha = 1;
+    }
+    setState(() {
+      appBarAlpha = alpha;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     //Scaffold 实现了基本的 Material Design 布局结构
     return Scaffold(
-      body: Center(
-        //显示轮播图
-        child: Column(
-          children: <Widget>[
-            //是一个结合了绘制（painting）、定位（positioning）以及尺寸（sizing）widget的widget。
-            Container(
-              height: 160,
-              //Swiper 轮播图组件
-              child: Swiper(
-                itemCount: _imageUrls.length, // 条目个数
-                autoplay: true, //自动播放
-                itemBuilder: (BuildContext context,index){
-                  //返回一个图片
-                  return Image.network(
-                    _imageUrls[index],
-                    fit: BoxFit.fill,//适配方式，填充父窗体
-                  );
+      //Stack 层叠组件，前面的元素在上面，后面的元素在下面
+      body: Stack(
+        children: <Widget>[
+          // MediaQuery 具有删除padding的属性
+          MediaQuery.removePadding(
+            //移除顶部padding
+              removeTop: true,
+              context: context,
+              //NotificationListener 可以监听列表的滚动
+              child:NotificationListener(
+                //监听列表的滚动
+                // ignore: missing_return
+                onNotification: (scrollNotification){
+                  //scrollNotification.depth 第0 个元素，也就是listview的第一个元素开始滚动的时候
+                  if(scrollNotification is ScrollNotification && scrollNotification.depth == 0){
+                    //滚动且是列表滚动的时候
+                    _onScroll(scrollNotification.metrics.pixels);
+
+                  }
                 },
-                //添加指示器
-                pagination: SwiperPagination(),
+                child: ListView(
+                  children: <Widget>[
+                    //是一个结合了绘制（painting）、定位（positioning）以及尺寸（sizing）widget的widget。
+                    Container(
+                      height: 160,
+                      //Swiper 轮播图组件
+                      child: Swiper(
+                        itemCount: _imageUrls.length, // 条目个数
+                        autoplay: true, //自动播放
+                        itemBuilder: (BuildContext context,index){
+                          //返回一个图片
+                          return Image.network(
+                            _imageUrls[index],
+                            fit: BoxFit.fill,//适配方式，填充父窗体
+                          );
+                        },
+                        //添加指示器
+                        pagination: SwiperPagination(),
+                      ),
+                    ),
+                    Container(
+                      height: 800,
+                      //ListTile 通常用于在 Flutter 中填充 ListView
+                      child: ListTile(title: Text('哈哈'),),
+                    )
+                  ],
+                ),
+              )
+          ),
+          //导航栏 Opacity包裹，可以改变组件透明度
+          Opacity(
+            //必传参数
+            opacity: appBarAlpha,
+            child: Container(
+              height: 80,
+              //decoration 装饰器, 背景色为白色
+              decoration: BoxDecoration(color: Colors.white,),
+              child: Center(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Text('首页'),
+                ),
               ),
-            )
-          ],
-        ),
-      ),
+            ),
+          )
+        ],
+      )
     );
   }
 }
